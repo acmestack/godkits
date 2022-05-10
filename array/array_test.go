f42684a4aeee6605988f8244f9a3407f76988741
Qicz <qiczzhu@gmail.com>
2022-05-10 21:47:28 +0800

    test: array test

---
+++ array/array_test.go
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022, OpeningO
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package array
+
+import "testing"
+
+func TestEmpty(t *testing.T) {
+	type args struct {
+		arr []any
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{arr: nil},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Empty(tt.args.arr...); got != tt.want {
+				t.Errorf("Empty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNotEmpty(t *testing.T) {
+	type args struct {
+		arr []any
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			args: args{arr: []any{"zcq", "z"}},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NotEmpty(tt.args.arr...); got != tt.want {
+				t.Errorf("NotEmpty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
