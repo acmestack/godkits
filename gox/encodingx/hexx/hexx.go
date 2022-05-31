04b4188098ddabe063cedfc6d5c890255d34deee
moremind <hefengen@hotmail.com>
2022-05-31 22:58:36 +0800

    [type:feat] commit hexx.go and hexx_test.go

---
+++ gox/encodingx/hexx/hexx.go
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022, OpeningO
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hexx
+
+import "encoding/hex"
+
+// HexEncodeToString convert bytes to string
+func HexEncodeToString(src []byte) string {
+	return hex.EncodeToString(src)
+}
+
+// HexDecodeToString build string to hex bytes
+func HexDecodeToString(src string) ([]byte, error) {
+	return hex.DecodeString(src)
+}
