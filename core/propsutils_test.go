d890071a7fa3741bc230223e8e03110b72094350
moremind <hefengen@hotmail.com>
2022-07-14 09:36:41 +0800

    [type:feat] support properties (#102)
    * [type:feat] support properties

--- core/propsutils_test.go
+++ core/propsutils_test.go
@@ -18,20 +18,20 @@
 package core
 
 import (
-	"github.com/acmestack/godkits/log"
-	uuid "github.com/satori/go.uuid"
+	"github.com/acmestack/godkits/assert"
 	"testing"
 )
 
-func TestGenUUIDRandom(t *testing.T) {
-	log.Info(UnixMillisId())
-	log.Info(UnixNanoTimeId())
-	log.Info(UUIDV1())
-	log.Info(UUIDV2('A'))
-	log.Info(UUIDV3(uuid.NamespaceOID, "test"))
-	log.Info(UUIDV5(uuid.NamespaceOID, "test"))
-	log.Info(RandomUUID())
-	log.Info(SimpleRandomUUID())
-	log.Info(ObjectId())
-	log.Info(ObjectIdWithNow())
+func TestGetConfigFromFile(t *testing.T) {
+	p := NewProperties()
+	_ = p.GetConfigFromFile("props.properties")
+	val, ok := p.Get("id")
+	assert.IsTrue(t, ok)
+	assert.Equal(t, 1, val)
+	val2, ok2 := p.Get("name")
+	assert.IsTrue(t, ok2)
+	assert.Equal(t, "xiaoming", val2)
+	val3, ok3 := p.Get("mm")
+	assert.IsFalse(t, ok3)
+	assert.IsTrue(t, val3 == "")
 }
