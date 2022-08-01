1cee1e96925846cabc277ab9cd6e2e5456884f0e
liuzwei <liuzhaowei.cool@163.com>
2022-08-01 14:50:14 +0800

    [type:feat]Add sha1,sha256,sha512

---
+++ gox/cryptox/sha512x/sha512x_test.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022, AcmeStack
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sha512x
+
+import "testing"
+
+func TestSha1(t *testing.T) {
+	testItems := []struct {
+		in  string
+		out string
+	}{
+		{
+			"aaa",
+			"d6f644b19812e97b5d871658d6d3400ecd4787faeb9b8990c1e7608288664be77257104a58d033bcf1a0e0945ff06468ebe53e2dff36e248424c7273117dac09",
+		},
+		{
+			"",
+			"cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e",
+		},
+	}
+
+	for _, item := range testItems {
+		if result := Sha512(item.in); result != item.out {
+			t.Errorf("Sha512() Error, want=%v, got=%v", item.out, result)
+		}
+	}
+}
